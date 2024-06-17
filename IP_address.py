b1093536e3f9bd88ab0fd70f17cf1aa2bee59e97
[Prasid69] <[aryalprasiddha999@gmail.com]>
2024-06-17 13:16:45 +0545

    adding new methods

--- IP_address.py
+++ IP_address.py
@@ -1,38 +1,64 @@
-import requests #here, it sends http requests
+import requests
 
 def get_ip():
     response = requests.get('https://api64.ipify.org/?format=json').json()
     return response["ip"]
 
-def get_location(ip_address):
-    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+def get_location(ip_address, api_key):
+    response = requests.get(f'https://ipinfo.io/{ip_address}?token={api_key}').json()
     location_data = {
         "ip": ip_address,
-        "city": response.get("city"),
-        "region": response.get("region"),
-        "country": response.get("country_name")
+        "City Name": response.get('city'),
+        "Region": response.get('region'),
+        "Country": response.get('country'),
+        "Time Zone": response.get('timezone')    
     }
     return location_data
 
-def check_vpn(ip_address, api_key):
-    url = f"https://vpnapi.io/api/{ip_address}?key={api_key}"
-    response = requests.get(url)
-    vpn_data = response.json()
-    return vpn_data
+def for_vpn_proxy(ip_address, api_key):
+    response = requests.get(f'https://vpnapi.io/api/{ip_address}?key={api_key}').json()
+    vpn_proxy = {
+        "ip": ip_address,
+        "VPN": response.get('security', {}).get('vpn'),
+        "Proxy": response.get('security', {}).get('proxy'),
+        "Tor": response.get('security', {}).get('tor')
+    }
+    return vpn_proxy
 
 def main():
-    api_key = 'cc1f480e5aa34bdb9119e243b46f9146'
-    user_ip = input("Enter an IP address (or press Enter to use your own IP): ")
+    ipinfo_api_key = 'a68557753f2cbd'  
+    vpnapi_api_key = 'cc1f480e5aa34bdb9119e243b46f9146'  
+    
+    user_ip = input("Enter an IP address (or press Enter to use your own IP): ") #current ko ip
     if not user_ip:
         user_ip = get_ip()
+
+    current_location = get_location(user_ip, ipinfo_api_key)
+    vpn_proxy_info = for_vpn_proxy(user_ip, vpnapi_api_key)
+   
+    previous_ip = input("Enter a previous IP address (or press Enter if not available): ") #previous ko
+    previous_country = None
+
+    if previous_ip:
+        previous_location = get_location(previous_ip, ipinfo_api_key)
+        previous_country = previous_location["Country"]
+
+    print("Geolocation Info:")
+    print(current_location)
     
-    vpn_info = check_vpn(user_ip, api_key)
-    print("\nVPN/Proxy Detection Info:")
-    print(vpn_info)
+    print("VPN and Proxy Info:")
+    print(vpn_proxy_info)
+    if vpn_proxy_info["VPN"]:
+        print("The IP address is using a VPN.")
+    if vpn_proxy_info["Proxy"]:
+        print("The IP address is using a Proxy.")
+    if vpn_proxy_info["Tor"]:
+        print("The IP address is using Tor.")
     
-    location = get_location(user_ip)
-    print("\nGeolocation Info:")
-    print(location)
+    if previous_country != current_location["Country"]:
+        print(f"The country has changed from {previous_country} to {current_location['Country']}.")
+    else:
+        print("The country has not changed.")
 
 if __name__ == "__main__":
     main()
